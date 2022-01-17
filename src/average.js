134c9318726ebc7b3033c6517ee5104eb429e6be
hellenribas <tchwtrabalho@gmail.com>
2022-01-17 16:59:33 -0300

    primeiro requisito ok

--- src/average.js
+++ src/average.js
@@ -12,6 +12,26 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const mediaCalc = (array) => {
+  let media = 0;
+  let sum = 0;
+  for (let elem of array) {
+    sum += elem;
+  }
+  media = Math.round(sum / array.length);
+  return media;
+};
 
+const average = (array) => {
+  if (array.length <= 0) {
+    return undefined;
+  }
+  for (let elem of array) {
+  if (typeof elem !== 'number') {
+    return undefined;
+  }
+}
+return mediaCalc(array);
+};
+console.log(average([]));
 module.exports = average;

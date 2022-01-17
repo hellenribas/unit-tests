a94cc66c94f6a7757a45f9087a8d97278c7b729b
hellenribas <tchwtrabalho@gmail.com>
2022-01-17 18:38:08 -0300

    requisito 5 ok

--- src/createStudent.js
+++ src/createStudent.js
@@ -14,7 +14,7 @@
     estudante.name // Retorna: 'Leandrão, o Lobo Solitário'
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
-
-const createStudent = () => {};
+const feedbacks = () => 'Eita pessoa boa!'; 
+const createStudent = (nome) => ({ name: nome, feedback: feedbacks });  
 
 module.exports = createStudent;

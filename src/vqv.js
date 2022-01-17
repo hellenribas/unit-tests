fed301133b8622cf5c75f6cc6bf71ac346c48ef9
hellenribas <tchwtrabalho@gmail.com>
2022-01-17 17:36:49 -0300

    requisito 3 ok

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,14 @@
       #VQV!'
 */
 
-const vqv = () => {};
-
+const vqv = (string, num) => {
+  if (string === undefined || num === undefined) {
+    return undefined;
+  }
+  return `Oi, meu nome é ${string}!
+Tenho ${num} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
+};
+  
 module.exports = vqv;

e4a74986e283f0123a7ac7f2f8b00cae76d14f11
hellenribas <tchwtrabalho@gmail.com>
2022-01-17 21:20:34 -0300

    requisito 8 ok

--- src/myCounter.js
+++ src/myCounter.js
@@ -18,13 +18,13 @@
 
 const myCounter = () => {
   var myArray = [];
-  for (var counter = 0; counter <= 3; counter += 1) {
-    myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
+  for (let counters = 0; counters <= 3; counters += 1) {
+    myArray.push(counters);
+    for (let counter = 2; counter <= 3; counter += 1) {
       myArray.push(counter);
     }
   }
   return myArray;
 };
-
+console.log(myCounter()); 
 module.exports = myCounter;
